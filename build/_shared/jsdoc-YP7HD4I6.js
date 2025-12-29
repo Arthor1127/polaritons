cf77dafc9c3612a2ead4802ec959780aba4a409d
Arthor1127 <arturo.ruiz@ib.edu.ar>
2025-12-29 03:37:06 -0400

    Update documentation

--- build/_shared/jsdoc-YP7HD4I6.js
+++ build/_shared/jsdoc-YP7HD4I6.js
@@ -1,1 +1,1 @@
-import{a}from"/build/_shared/chunk-VZXSIKCR.js";import"/build/_shared/chunk-XK3K4AT7.js";import"/build/_shared/chunk-MRS6DOXX.js";import"/build/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/polaritons/build/_shared/chunk-VZXSIKCR.js";import"/polaritons/build/_shared/chunk-XK3K4AT7.js";import"/polaritons/build/_shared/chunk-MRS6DOXX.js";import"/polaritons/build/_shared/chunk-OZE3FFNP.js";export default a();
